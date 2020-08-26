dcb55f9b9a2a618d4614fac4b4a93018ce861d5a
Rohit Kunji <srinivasan.kunji@gmail.com>
2020-08-27 05:18:29 +0530

    Add Services card

--- src/Components/Services/Card.js
+++ src/Components/Services/Card.js
@@ -3,21 +3,21 @@ import styles from './Card.module.css';
 
 function Card(props) {
 	return(
-		<div>
-			<div className={styles.cards}>
-				<div className={styles.card}>
-					<img src={props.imgsrc} alt="pic" className={styles.cardimg} />
-					<div className={styles.cardinfo}>
-						<h3 className={styles.heading}>
-							{props.head}
-						</h3>
-						<h5 className={styles.subtext}>
-							{props.bod}
-						</h5>
-					</div>
-				</div>
+	<div className={styles.cards}>
+		<div className={styles.card}>
+			<div className={styles.cardinfo}>
+				<h3 className={styles.heading}>
+					{props.head}
+				</h3>
+				<h5 className={styles.subtext}>
+					{props.bod}
+				</h5>
+				<h5 className={styles.learn}>
+					Learn More
+				</h5>
 			</div>
 		</div>
+	</div>
 	);
 }
 
--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -1,41 +1,46 @@
 import React from 'react';
 import styles from './Services.module.css';
 import Card from './Card';
-import Home from '../../assets/home.png';
+import Home from '../../assets/hero.jpg';
 
 function Services() {
 	return(
-		<div >
-			<Card 
-				imgsrc={Home}
-				head="Mobile Development"
-				bod="This is rare to find. We, at ThumbStack, pick the finest App developers with an upper hand in technical as well as non-technical skills."
-			/>
-			<Card 
-				imgsrc={Home}
-				head="Mobile Development"
-				bod="This is rare to find. We, at ThumbStack, pick the finest App developers with an upper hand in technical as well as non-technical skills."
-			/>
-			<Card 
-				imgsrc={Home}
-				head="Mobile Development"
-				bod="This is rare to find. We, at ThumbStack, pick the finest App developers with an upper hand in technical as well as non-technical skills."
-			/>
-			<Card 
-				imgsrc={Home}
-				head="Mobile Development"
-				bod="This is rare to find. We, at ThumbStack, pick the finest App developers with an upper hand in technical as well as non-technical skills."
-			/>
-			<Card 
-				imgsrc={Home}
-				head="Mobile Development"
-				bod="This is rare to find. We, at ThumbStack, pick the finest App developers with an upper hand in technical as well as non-technical skills."
-			/>
-			<Card 
-				imgsrc={Home}
-				head="Mobile Development"
-				bod="This is rare to find. We, at ThumbStack, pick the finest App developers with an upper hand in technical as well as non-technical skills."
-			/>
+		<div>
+			<div className={styles.head}>
+				<h1 className={styles.heading}>Services</h1>
+			</div>
+			<div className={styles.cards}>
+				<Card 
+					imgsrc={Home}
+					head="Mobile Development"
+					bod="This is rare to find. We, at ThumbStack, pick the finest App developers with an upper hand in technical as well as non-technical skills."
+				/>
+				<Card 
+					imgsrc={Home}
+					head="Mobile Development"
+					bod="This is rare to find. We, at ThumbStack, pick the finest App developers with an upper hand in technical as well as non-technical skills."
+				/>
+				<Card 
+					imgsrc={Home}
+					head="Mobile Development"
+					bod="This is rare to find. We, at ThumbStack, pick the finest App developers with an upper hand in technical as well as non-technical skills."
+				/>
+				<Card 
+					imgsrc={Home}
+					head="Mobile Development"
+					bod="This is rare to find. We, at ThumbStack, pick the finest App developers with an upper hand in technical as well as non-technical skills."
+				/>
+				<Card 
+					imgsrc={Home}
+					head="Mobile Development"
+					bod="This is rare to find. We, at ThumbStack, pick the finest App developers with an upper hand in technical as well as non-technical skills."
+				/>
+				<Card 
+					imgsrc={Home}
+					head="Mobile Development"
+					bod="This is rare to find. We, at ThumbStack, pick the finest App developers with an upper hand in technical as well as non-technical skills."
+				/>
+			</div>
 		</div>
 	);
 }
