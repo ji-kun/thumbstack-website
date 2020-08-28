7fc8aeecb271e193896b4bceeb246ace42c8f0c5
Rohit Kunji <srinivasan.kunji@gmail.com>
2020-08-29 05:24:09 +0530

    Updates

---
+++ precache-manifest.aa24d2b9d425b67b4a67757f12a923c7.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44c720e90ed2e3d85ca8d84549a19c6a",
+    "url": "/thumbstack-website/index.html"
+  },
+  {
+    "revision": "65510284a5b4af0b3cf5",
+    "url": "/thumbstack-website/static/css/main.ec152ff7.chunk.css"
+  },
+  {
+    "revision": "eeb2bfc7d0fa2accb63e",
+    "url": "/thumbstack-website/static/js/2.def7074b.chunk.js"
+  },
+  {
+    "revision": "0ff773abdb301003272581a2918ee4a1",
+    "url": "/thumbstack-website/static/js/2.def7074b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65510284a5b4af0b3cf5",
+    "url": "/thumbstack-website/static/js/main.635dd003.chunk.js"
+  },
+  {
+    "revision": "d6d79e73096b1504f8bc",
+    "url": "/thumbstack-website/static/js/runtime-main.69b97a58.js"
+  },
+  {
+    "revision": "7200ef72b24710deffcdc5aea0499835",
+    "url": "/thumbstack-website/static/media/Group 7.7200ef72.png"
+  },
+  {
+    "revision": "be60abec3374150eb2a82f3fd1d6fd24",
+    "url": "/thumbstack-website/static/media/Group 8.be60abec.png"
+  },
+  {
+    "revision": "057339a2dbff45c22c4583d668a4a395",
+    "url": "/thumbstack-website/static/media/Group8.057339a2.jpg"
+  },
+  {
+    "revision": "c2e71451a54d6eca52a7b603313c42b2",
+    "url": "/thumbstack-website/static/media/Group9.c2e71451.jpg"
+  },
+  {
+    "revision": "144d70af2c532881e7f99677cccd0e8c",
+    "url": "/thumbstack-website/static/media/bv.144d70af.png"
+  },
+  {
+    "revision": "f5c7d25bedfa19506152e067a1e74de3",
+    "url": "/thumbstack-website/static/media/dbs.f5c7d25b.png"
+  },
+  {
+    "revision": "82b2717270d55452550402afe90fc43d",
+    "url": "/thumbstack-website/static/media/facebook.82b27172.svg"
+  },
+  {
+    "revision": "cf27169e3b1458b67cf82feec8fe529f",
+    "url": "/thumbstack-website/static/media/group.cf27169e.png"
+  },
+  {
+    "revision": "c14e17cd7a501b3fc7625358550cf074",
+    "url": "/thumbstack-website/static/media/hero.c14e17cd.jpg"
+  },
+  {
+    "revision": "d9db32e9dff402200ade4e935394d83d",
+    "url": "/thumbstack-website/static/media/home.d9db32e9.png"
+  },
+  {
+    "revision": "27fe8f952cd2a056129b54d59ed6f815",
+    "url": "/thumbstack-website/static/media/indu.27fe8f95.png"
+  },
+  {
+    "revision": "ea1bb834115f1cab447be219b0f21e4a",
+    "url": "/thumbstack-website/static/media/instagram.ea1bb834.svg"
+  },
+  {
+    "revision": "6934d4c4f57d65b6ee3afbfa8d1cec63",
+    "url": "/thumbstack-website/static/media/linkedin.6934d4c4.svg"
+  },
+  {
+    "revision": "f991f51a1965cf499f3531ed7db4d107",
+    "url": "/thumbstack-website/static/media/logo.f991f51a.png"
+  },
+  {
+    "revision": "40134cc613dd0686de21b8d9613f6b67",
+    "url": "/thumbstack-website/static/media/maskGroup.40134cc6.png"
+  },
+  {
+    "revision": "4b53bcacf1af942b50c55d090b514efa",
+    "url": "/thumbstack-website/static/media/mountmusic.4b53bcac.png"
+  },
+  {
+    "revision": "f3fe85a4056c62daea4ddf4649acccb3",
+    "url": "/thumbstack-website/static/media/nexus.f3fe85a4.png"
+  },
+  {
+    "revision": "fe777fd739f414a5963133fa6de22638",
+    "url": "/thumbstack-website/static/media/pristine.fe777fd7.png"
+  }
+]);
